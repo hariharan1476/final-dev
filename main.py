aa5713db27b5e11857e5b7dafb5518edef02c402
HARIHARAN K <140387416+hariharan1476@users.noreply.github.com>
2025-07-30 05:53:55 +0000

    devopsss

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def add(x,y):
   return x+y
 if __name__=="__main__":
-  print(add(2,3))
+  print(add(2,6))
--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
 
 from main import add
 def test_add():
-  assert add(2,3)==5
+  assert add(2,6)==8
+
